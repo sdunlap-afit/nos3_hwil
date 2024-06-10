8b7afa302dd5a9928f62efbee00d31517431a974
sdunlap <sdunlap42@gmail.com>
2024-06-10 19:13:05 +0000

    Updated to dev branch of deployment

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,34 +1,17 @@
 
 
 sudo apt update
-sudo apt install -y make cmake
 
-echo '10.1.10.52 nos_engine_server' | sudo tee -a /etc/hosts
 
-# Dependencies from Deployment Dockerfile
-sudo apt install -y \
-        cmake \
-        git \
-		gdb \
-        python3-dev \
-        python3-pip \
-        dwarves \
-        freeglut3-dev \
-        libboost-dev \
-        libboost-system-dev \
-        libboost-program-options-dev \
-        libboost-filesystem-dev \
-        libboost-thread-dev \
-        libboost-regex-dev \
-        libgtest-dev \
-        libicu-dev \
-        libncurses5-dev \
-        libreadline-dev \
-        libsocketcan-dev \
-        libxerces-c-dev \
-        wget 
 
-        # g++-multilib \
-        # lib32z1 \
-        # python-apt \
-        # gcc-multilib \
+
+cd nos3
+make config
+make build-fsw
+
+# echo '10.1.10.52 nos_engine_server' | sudo tee -a /etc/hosts
+
+
+# Needs tested
+# echo '* - rtprio 99' | sudo tee -a /etc/security/limits.conf
+# Reboot
