7bb526d46676461ddcbc88b84d090c7ab119221d
sdunlap <sdunlap42@gmail.com>
2024-06-10 08:26:42 -0400

    Updates

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,6 +1,34 @@
 
 
+# sudo apt install -y make cmake
 
-echo '10.1.10.52 nos_engine_server' | sudo tee -a /etc/hosts
+# echo '10.1.10.52 nos_engine_server' | sudo tee -a /etc/hosts
 
 
+sudo apt update
+sudo apt install -y \
+        cmake \
+        git \
+		gdb \
+        python3-dev \
+        python3-pip \
+        dwarves \
+        freeglut3-dev \
+        libboost-dev \
+        libboost-system-dev \
+        libboost-program-options-dev \
+        libboost-filesystem-dev \
+        libboost-thread-dev \
+        libboost-regex-dev \
+        libgtest-dev \
+        libicu-dev \
+        libncurses5-dev \
+        libreadline-dev \
+        libsocketcan-dev \
+        libxerces-c-dev \
+        wget 
+
+        # g++-multilib \
+        # lib32z1 \
+        # python-apt \
+        # gcc-multilib \
